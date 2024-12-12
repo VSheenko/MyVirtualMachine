4dfcd540a69c2970bb1d84d12e590ce5534d6e20
VSheenko <vovan072005@gmail.com>
2024-12-12 19:52:18 +0300

    Некоторая реализация работы с памятью

--- sources/Machine/Memory/Memory.cpp
+++ sources/Machine/Memory/Memory.cpp
@@ -1,1 +1,35 @@
+#include <stdexcept>
 #include "Memory.h"
+
+Memory::Memory(uint16_t size) {
+    memory.resize(size);
+    reset();
+}
+
+
+void Memory::reset() {
+    std::fill(memory.begin(), memory.end(), 0);
+}
+
+std::vector<uint8_t> Memory::read(uint16_t address, uint16_t size) {
+    if (address + size > memory.size()) {
+        throw std::out_of_range("Memory out of bounds");
+    }
+
+    std::vector<uint8_t> res(size);
+    std::copy(memory.begin() + address, memory.begin() + address + size, res.begin());
+
+    return res;
+}
+
+void Memory::write(uint16_t address, const std::vector<uint8_t> &data) {
+    if (address + data.size() > memory.size()) {
+        throw std::out_of_range("Memory out of bounds");
+    }
+
+    std::copy(data.begin(), data.end(), memory.begin() + address);
+}
+
+uint16_t Memory::size() {
+    return memory.size();
+}
--- sources/Machine/Memory/Memory.h
+++ sources/Machine/Memory/Memory.h
@@ -1,9 +1,21 @@
 #ifndef MYVIRTUALMACHINE_MEMORY_H
 #define MYVIRTUALMACHINE_MEMORY_H
 
+#include <cstdint>
+#include <vector>
 
 class Memory {
+private:
+    std::vector<uint8_t> memory;
 
+public:
+    explicit Memory(uint16_t size);
+    ~Memory() = default;
+
+    void reset();
+    uint16_t size();
+    void write(uint16_t address, const std::vector<uint8_t>& data);
+    std::vector<uint8_t> read(uint16_t address, uint16_t size);
 };
 
 
