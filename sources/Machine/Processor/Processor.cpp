b6f47421f5b5328b325f2158d4f84e7f0bc23c99
VSheenko <vovan072005@gmail.com>
2024-12-16 17:44:32 +0300

    Некоторые команды процессора и структура регистров

--- sources/Machine/Processor/Processor.cpp
+++ sources/Machine/Processor/Processor.cpp
@@ -1,5 +1,69 @@
-//
-// Created by vovan on 11.12.2024.
-//
-
+#include <stdexcept>
 #include "Processor.h"
+
+void regs::reset() {
+    PC = 0;
+    IR = 0;
+    ACC = 0;
+
+    for (uint32_t & i : R) {
+        i = 0;
+    }
+
+    FLAGS = 0;
+}
+
+void Processor::mov(uint32_t& dst, uint32_t src) {
+
+}
+
+void Processor::push(uint32_t src) {
+    stack.push(src);
+}
+
+void Processor::pop(uint32_t &dst) {
+    if (stack.empty()) {
+        return;
+    }
+
+    dst = stack.top();
+    stack.pop();
+}
+
+void Processor::xchg(uint32_t &dst, uint32_t &src) {
+    uint32_t tmp = dst;
+    dst = src;
+    src = tmp;
+}
+
+void Processor::add(uint32_t &dst, uint32_t src) {
+    dst += src;
+}
+
+void Processor::sub(uint32_t &dst, uint32_t src) {
+    dst -= src;
+}
+
+void Processor::inc(uint32_t &dst) {
+    dst++;
+}
+
+void Processor::dec(uint32_t &dst) {
+    dst--;
+}
+
+void Processor::mul(uint32_t &dst, uint32_t src) {
+    dst *= src;
+}
+
+void Processor::div(uint32_t &dst, uint32_t src) {
+    if (src == 0) {
+        throw std::runtime_error("Division by zero");
+    }
+
+    dst /= src;
+}
+
+void Processor::neg(uint32_t &dst) {
+    dst = -dst;
+}
--- sources/Machine/Processor/Processor.h
+++ sources/Machine/Processor/Processor.h
@@ -1,8 +1,55 @@
 #ifndef MYVIRTUALMACHINE_PROCESSOR_H
 #define MYVIRTUALMACHINE_PROCESSOR_H
 
+#include <cstdint>
+#include <stack>
+
+struct regs {
+    uint32_t PC = 0;
+    uint32_t IR = 0;
+    uint32_t ACC = 0;
+    uint32_t R[8] = {0};
+
+    uint32_t FLAGS = 0;
+
+    void reset();
+};
+
+enum Opcode {
+    MOV = 0x8A,
+    PUSH = 0x50,
+    POP = 0x58,
+    XCHG = 0x86,
+
+    ADD = 0x01,
+    SUB = 0x2B,
+    INC = 0x40,
+    DEC = 0x48,
+    MUL = 0xF6,
+    DIV = 0xF7,
+    NEG = 0xF8,
+
+    AND = 0x21,
+    OR = 0x09,
+    XOR = 0x31,
+    NOT = 0xF9,
+};
 
 class Processor {
+private:
+    std::stack<uint16_t> stack;
+
+    void mov(uint32_t& dst, uint32_t src);
+    void push(uint32_t src);
+    void pop(uint32_t& dst);
+    void xchg(uint32_t& dst, uint32_t& src);
+    void add(uint32_t& dst, uint32_t src);
+    void sub(uint32_t& dst, uint32_t src);
+    void inc(uint32_t& dst);
+    void dec(uint32_t& dst);
+    void mul(uint32_t& dst, uint32_t src);
+    void div(uint32_t& dst, uint32_t src);
+    void neg(uint32_t& dst);
 
 };
 
