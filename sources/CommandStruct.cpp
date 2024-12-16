6a1a2c869024507d6fd7f27dd77a94a4dcd81f5d
VSheenko <vovan072005@gmail.com>
2024-12-16 19:55:28 +0300

    Новая структура команд для процессора и asm

---
+++ sources/CommandStruct.cpp
@@ -0,0 +1,66 @@
+#include <stdexcept>
+#include <regex>
+#include "CommandStruct.h"
+
+std::unordered_map<std::string, uint8_t> CommandStruct::opcodeMap = {
+        {"MOV", 0x8A},
+        {"PUSH", 0x50},
+        {"POP", 0x58},
+        {"XCHG", 0x86},
+
+        {"ADD", 0x01},
+        {"SUB", 0x2B},
+        {"INC", 0x40},
+        {"DEC", 0x48},
+        {"MUL", 0xF6},
+        {"DIV", 0xF7},
+
+        {"NEG", 0xF8},
+        {"AND", 0x21},
+        {"OR", 0x09},
+        {"XOR", 0x31},
+        {"NOT", 0xF9}
+};
+
+CommandStruct::CommandStruct() : opcode(0), addrMode(0), operand1(0), operand2(0) {
+
+}
+
+std::vector<uint8_t> CommandStruct::GetBinFormat() const {
+    std::vector<uint8_t> res;
+    res.push_back(opcode);
+    res.push_back(addrMode);
+    res.push_back(operand1 & 0xFF);
+    res.push_back((operand1 >> 8) & 0xFF);
+    res.push_back((operand1 >> 16) & 0xFF);
+    res.push_back((operand1 >> 24) & 0xFF);
+    res.push_back(operand2 & 0xFF);
+    res.push_back((operand2 >> 8) & 0xFF);
+    res.push_back((operand2 >> 16) & 0xFF);
+    res.push_back((operand2 >> 24) & 0xFF);
+    return res;
+}
+
+void CommandStruct::SetFromBinFormat(std::vector<uint8_t> bin) {
+    opcode = bin[0];
+    addrMode = bin[1];
+    operand1 = bin[2] | (bin[3] << 8) | (bin[4] << 16) | (bin[5] << 24);
+    operand2 = bin[6] | (bin[7] << 8) | (bin[8] << 16) | (bin[9] << 24);
+}
+
+uint8_t CommandStruct::GetOpcode(std::string command) {
+    if (opcodeMap.find(command) == opcodeMap.end()) {
+        throw std::runtime_error("Unknown command");
+    }
+
+    return opcodeMap.at(command);
+}
+
+void CommandStruct::reset() {
+    opcode = 0;
+    addrMode = 0;
+    operand1 = 0;
+    operand2 = 0;
+}
+
+
---
+++ sources/CommandStruct.h
@@ -0,0 +1,25 @@
+#ifndef MYVIRTUALMACHINE_COMMANDSTRUCT_H
+#define MYVIRTUALMACHINE_COMMANDSTRUCT_H
+
+#include <unordered_map>
+#include <cstdint>
+#include <vector>
+#include <string>
+
+struct CommandStruct {
+    uint8_t opcode;
+    uint8_t addrMode;
+    uint32_t operand1;
+    uint32_t operand2;
+
+    CommandStruct();
+    [[nodiscard]] std::vector<uint8_t> GetBinFormat() const;
+    void SetFromBinFormat(std::vector<uint8_t> bin);
+    static uint8_t GetOpcode(std::string command);
+    void reset();
+
+private:
+    static std::unordered_map<std::string, uint8_t> opcodeMap;
+};
+
+#endif //MYVIRTUALMACHINE_COMMANDSTRUCT_H
